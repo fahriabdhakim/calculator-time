afc4a479b96274097c8f5633134b34b19491d535
Fahri Abdul Hakim <fahriabdhakim@yahoo.com>
2018-01-16 16:09:23 +0700

    add getElementById

---
+++ index.js
@@ -0,0 +1,12 @@
+var date = new Date();
+var minute = date.getMinutes();
+var hour = date.getHours();
+var day = date.getDate();
+
+console.log(date);
+console.log(hour);
+console.log(minute);
+
+document.getElementById('fahri').textContent = date
+document.getElementById('fahri').value
+document.querySelectorAll('#id')
